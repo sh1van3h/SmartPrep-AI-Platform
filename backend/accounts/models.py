fe24f7568c87d57fca73592b0a8cce13cd269ac8
Sh1van3h <spparmar234234@gmail.com>
2026-07-05 19:24:37 +0530

    Register Subject model in Django admin

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Subjects(models.Model):
+class Subject(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
