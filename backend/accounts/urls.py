e19d0a9acce56d7d7cb5a92c19b8a75dc7aca6f8
Sh1van3h <spparmar234234@gmail.com>
2026-07-06 15:44:32 +0530

    Implement note creation with form and database save

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("subjects/", views.subject_list),
-    path("subjects/<int:id>/", views.subject_detail),
+    path("subjects/", views.subject_list, name="subject_list"),
+    path("subjects/<int:id>/", views.subject_detail, name="subject_detail"),
+    path("subjects/<int:id>/add-note", views.add_note, name="add_note"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render 
+from django.shortcuts import render , get_object_or_404
 from .models import Subject , Note
-from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 # Create your views here.
 
 def subject_list(request):
@@ -21,5 +21,22 @@ def subject_detail(request, id):
         {
             "subject" : subject,
             "notes" : notes,
+        }
+    )
+
+def add_note(request, id):
+    subject = get_object_or_404(Subject, id=id)
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        Note.objects.create(title=title,content=content,
+                            subject=subject)
+        return redirect("subject_detail" , id=subject.id)
+    
+    return render(
+        request,
+        "accounts/add-note.html",
+        {
+            "subject" : subject
         }
     )
